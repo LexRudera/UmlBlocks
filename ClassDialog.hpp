279d457371b3bf5c9bb04b48e00526a33220a093
Lex Rudera <lex@pulsewave.co>
2013-08-04 02:28:58 +0200

    Recreated the ClassDialog
    This time with an XML layout file, instead of hardcoded stuff.

--- ClassDialog.hpp
+++ ClassDialog.hpp
@@ -1,81 +1,31 @@
-#ifndef ClassDIALOG_HPP
-#define ClassDIALOG_HPP
+#ifndef CLASSDIALOG_H
+#define CLASSDIALOG_H
 
-#include <sdk.h>
-#include <list>
-//#include <wx/string.h>
-#include <wx/Dialog.h>
-#include <wx/statbox.h>
-#include <wx/textctrl.h>
-#include "MemberFunc.hpp"
-#include "MemberVar.hpp"
-class ClassDialog : public wxDialog
+//(*Headers(ClassDialog)
+#include <wx/dialog.h>
+//*)
+
+class ClassDialog: public wxDialog
 {
+	public:
 
-    public:
-        /** Default constructor */
-        ClassDialog();
-        /** Default destructor */
-        virtual ~ClassDialog();
-        /** Copy constructor
-         *  \param other Object to copy from
-         */
-        ClassDialog(const ClassDialog& other);
-        /** Assignment operator
-         *  \param other Object to assign from
-         *  \return A reference to this
-         */
-        ClassDialog& operator=(const ClassDialog& other);
-    protected:
-    private:
-        void Create();
-        void CreateControls();
-        // Member C-style arrays is pure virtual hell. Har har. very funny. But seriously, what the shit.
-        //wxString AccessOptions[];
+		ClassDialog(wxWindow* parent);
+		virtual ~ClassDialog();
 
-        std::list<MemberFunc> Functions;
-        std::list<MemberVar> Variables;
+		//(*Declarations(ClassDialog)
+		//*)
 
-        wxStaticBox* DeclareSBox;
-        wxTextCtrl* ClassNameBox;
-        wxTextCtrl* DescriptionBox;
+	protected:
 
-        wxStaticBox* InheritSBox;
-        wxListBox* InheritList;
-        wxChoice* InheriSelector;
-        wxTextCtrl* InheriExternal;
-        wxChoice* InheriAccess;
+		//(*Identifiers(ClassDialog)
+		//*)
 
-        wxStaticBox* AttribuSBox;
-        wxCheckBox* AbstractCheckbox;
+	private:
 
-        wxStaticBox* MemVariSBox;
-        wxListBox* VarList;
-        wxTextCtrl* VarName;
-        wxTextCtrl* VarType;
-        wxChoice* VarAccess;
-        wxCheckBox* VarReference;
-        wxCheckBox* VarPointer;
-        wxSpinCtrl* VarPointAmount;
-        wxCheckBox* VarConstant;
-        wxCheckBox* VarStatic;
+		//(*Handlers(ClassDialog)
+		//*)
 
-        wxStaticBox* MemFuncSBox;
-        wxListBox* FuncList;
-        wxTextCtrl* FuncName;
-        wxTextCtrl* FuncType;
-        wxChoice* FuncAccess;
-            wxStaticBox* FuncParameterSubBox;
-            wxListBox* ParaList;
-            wxTextCtrl* ParaName;
-            wxTextCtrl* ParaType;
-            wxCheckBox* ParaReference;
-            wxCheckBox* ParaPointer;
-            wxSpinCtrl* ParaPointAmount;
-            wxCheckBox* ParaConstant;
-            wxTextCtrl* ParaDefaultArg;
-        wxCheckBox* FuncVirtual;
-        wxCheckBox* FuncPure;
+		DECLARE_EVENT_TABLE()
 };
 
-#endif // ClassDIALOG_HPP
+#endif
